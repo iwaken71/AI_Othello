67dbe1e3c8fc3b50f54e0b16bdc7e6bcc0f097f7
iwaken71 <iwaken59371518@gmail.com>
2025-04-15 00:29:41 +0900

    github page設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI_Othello/' // リポジトリ名に完全に一致させる（大文字小文字も含めて）
 })
